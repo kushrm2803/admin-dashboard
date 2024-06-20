f751cbc049ea6ffcd12e46cd1db081e97e03d494
SwapnilPandey2210 <2022csb1133@iitrpr.ac.in>
2024-06-21 04:12:29 +0530

    Theme(dark and light)

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,17 @@
+import{ CssBaseline, ThemeProvider } from "amui/material";
+import{ createTheme } from "amui/material/styles"; 
+import { useMemo } from "react"; 
+import { useSelector } from "react-redux";
+import { themeSettings } from "theme";
+
 function App() {
+  const mode = useSelector ((state) => state.global.mode) ;
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="App">
-      
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
 }
